f53057f702b3f4472d37555ea12f67e2fc014bd7
LuisGuiCv <96149820+LuisGuiCv@users.noreply.github.com>
2022-01-17 20:01:46 -0600

    agrego ejercicios de Matrices y bucles

---
+++ Matrices_y_bucles/ejercicios_matrices.js
@@ -0,0 +1,66 @@
+//Exercise 1
+/*
+//Part One
+var people=["Sofía","David","Juan"];
+
+console.log(people);
+
+
+//Part 2
+
+people.push("Sara","Agustin");
+people.shift();
+console.log(people);
+
+people.splice(2,0,"Renata");
+people.push("Elena");
+console.log(people);
+*/
+//Exercise 2
+
+//Declaración del array 
+/*
+var matriz=new Array();
+
+for (let i = 0; i <=4; i++) {
+    matriz[i]=new Array();
+    for (let j = 0; j <= i; j++) {
+        matriz[i][j]="*";
+    }
+}
+console.log(matriz);
+*/
+
+//Exercise 3
+
+//Part One
+/*
+let x=4750;
+
+while(x>=0){
+    console.log(x);
+    x -=0.5;
+
+}*/
+
+//Part Two
+/*
+let oddnumbers=1
+
+while(oddnumbers<100){
+    console.log(oddnumbers);
+    oddnumbers += 2;
+
+}
+*/
+
+let suma=0;
+let n=3;
+let i=0;
+while(i<5){
+    suma +=n;
+    i++;
+}
+console.log(suma);
+
+
